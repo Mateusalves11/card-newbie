19717f93a3cc7d3b7a7d9d49894ca79433a8fe43
Mateus Alves <mateuslkkls@gmail.com>
2022-12-29 11:24:31 -0300

    feat(criar endpoint do card com useState)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import { Card } from "components/Card/index";
 import { Container } from "components/Container";
+import { useState } from "react";
+
 
 function App() {
+  const [componentsData] = useState([{
+    card: [{
+      category: "Perfume",
+      title: "Gabrielle Essence Eau de Parfum",
+      description: "A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.",
+      price: "$149.99",
+      prevPrice: "$169.99",
+      pathImageDesktop: "CardAssets/image-product-desktop.jpg",
+      pathImageMobile: "CardAssets/image-product-mobile.jpg",
+    }]
+  }]);
+
   return (
     <Container>
-      <Card />
+      <Card data={componentsData} />
     </Container>
   );
 }
